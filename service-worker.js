7f22006ea19456d99c182235bb7795c10c6979dd
TheGri <86673666+TheGri@users.noreply.github.com>
2021-07-05 22:40:54 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,101 @@
-var cacheName = 'HTWK-K';
-var filesToCache = [
-	'./index.html',
-	'./images/logo512.png'
-];
+var cacheName = "HTWK-K";
+var filesToCache = ["./index.html", "./images/logo512.png", "./offline.html"];
 
-// Unterschied self. vs. this. ???
-self.addEventListener('install', function(pEvent) {
-	console.log('[Service Worker] Installation wird gestartet');
-	pEvent.waitUntil(
-		caches.open(cacheName).then(function(pCache) {
-			console.log('[Service Worker] Dateien werden gecacht');
-			return pCache.addAll(filesToCache);
-		})
-	);
+// Hinzufügen Install Event-Listener
+this.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installation wird gestartet");
+  event.waitUntil(
+    //SW wird nicht installiert solange folgender Code nicht erfolgreich ausgegführt
+    caches.open(cacheName).then(function (cache) {
+      //Cache öffnen
+      console.log("[Service Worker] Dateien werden gecacht");
+      return cache.addAll(filesToCache); //Cache werden Ressourcen hinzugefügt
+    })
+  );
 });
 
-self.addEventListener('activate', function(pEvent) {
-	console.log('[Service Worker] Aktivierung wird gestartet');
-	pEvent.waitUntil(
-		caches.keys().then(function(pKeys) {
-			return Promise.all(pKeys.map(function(pKey) {
-				if (pKey !== cacheName) {
-					console.log('[Service Worker] Veralteter Cache wird gelöscht:', pKey);
-					return caches.delete(pKey);
-				}
-			}));
-		})
-	);
+this.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Aktivierung wird gestartet");
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== cacheName) {
+            console.log(
+              "[Service Worker] Veralteter Cache wird gelöscht:",
+              key
+            );
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch -> was sollen SW mit gecachcten Inhalten tun
+
+this.addEventListener("fetch", function (event) {
+  event.respondWith(
+    // jede Ressource die vom Netzwerk angefragt wird, mit der des Cache ersetzen falls verfügbar
+    caches
+      .match(event.request)
+      .then(function (resp) {
+        return (
+          resp ||
+          fetch(event.request).then(function (response) {
+            //caches.open(cacheName).then(function(cache) {
+            //	cache.put(event.request, response.clone());
+            //});
+            return response;
+          })
+        );
+      })
+      .catch(function () {
+        return caches.match("/offline.html");
+      })
+  );
 });
 
 /* Stale-while-revalidate Ansatz
  * Seiten müssen nicht aktuellste sein, werden aus Cache geladen
  * im Hintergrund wird offline nach aktuellerer Variante gesucht
  */
-self.addEventListener('fetch', (event) => {
-	console.log("[Service Worker] Angeforderte URL:", event.request.url);
-	console.log("Die Ressource wurde aus dem Cache geladen und es wird im Netzwerk nach einer neuen Variante gesucht.");
-	event.respondWith(async function() {
-		const cache = await caches.open(cacheName);
-		const cachedResponse = await cache.match(event.request);
-		const networkResponsePromise = fetch(event.request);
-		event.waitUntil(async function() {
-			const networkResponse = await networkResponsePromise;
-			await cache.put(event.request, networkResponse.clone());
-		}());
-		// Return the cached response if we have one, otherwise return the network response.
-		if (cachedResponse) return cachedResponse;
+/*self.addEventListener("fetch", (event) => {
+  console.log("[Service Worker] Angeforderte URL:", event.request.url);
+  //console.log("Die Ressource wurde aus dem Cache geladen und es wird im Netzwerk nach einer neuen Variante gesucht.");
+  event.respondWith(
+    (async function () {
+      try {
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(event.request);
+        const networkResponsePromise = fetch(event.request);
 
-		try {
-			return await networkResponsePromise;
-		} catch (err) {
-			return caches.match("/offline.html");
-		}
-	}());
+        event.waitUntil(
+          (async function () {
+            const networkResponse = await networkResponsePromise;
+            await cache.put(event.request, networkResponse.clone());
+          })()
+        );
+
+        // Return the cached response if we have one, otherwise return the network response.
+        return cachedResponse || networkResponsePromise;
+      } catch (err) {
+		  console.log("Fetch fehlgeschlagen");
+        return cache.match("offline.html");
+      }
+    })()
+  );
 });
-/*	pEvent.respondWith(
-		caches.match(pEvent.request).then(function(pResponse) {
-			if (pResponse) {
-				console.log('[Service Worker] Ressource konnte aus dem Cache geladen werden:', pEvent.request.url);
-				return pResponse;
+
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			if (response) {
+				console.log('[Service Worker] Ressource konnte aus dem Cache geladen werden:', event.request.url);
+				return response;
 			} else {
-				console.log('[Service Worker] Ressource wird aus dem Netzwerk geladen:', pEvent.request.url);
-				return fetch(pEvent.request);
+				console.log('[Service Worker] Ressource wird aus dem Netzwerk geladen:', event.request.url);
+				return fetch(event.request);
 			}
 		})
 	);
@@ -69,10 +103,10 @@ self.addEventListener('fetch', (event) => {
 
 // Background Sync
 self.addEventListener("sync", (event) => {
-	event.waitUntil(
-	  (async function () {
-		const cache = await caches.open(cacheName);
-		await cache.add(filesToCache);
-	  })()
-	);
-  });
+  event.waitUntil(
+    (async function () {
+      const cache = await caches.open(cacheName);
+      await cache.add(filesToCache);
+    })()
+  );
+});
