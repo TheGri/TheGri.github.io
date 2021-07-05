cc686ac8a5e30ba04a2488060cd01e9064740a77
TheGri <86673666+TheGri@users.noreply.github.com>
2021-07-05 10:26:45 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,17 @@ var filesToCache = [
 	'./images/logo512.png'
 ];
 
+// Unterschied self. vs. this. ???
+self.addEventListener('install', function(pEvent) {
+	console.log('[Service Worker] Installation wird gestartet');
+	pEvent.waitUntil(
+		caches.open(cacheName).then(function(pCache) {
+			console.log('[Service Worker] Dateien werden gecacht');
+			return pCache.addAll(filesToCache);
+		})
+	);
+});
+
 self.addEventListener('activate', function(pEvent) {
 	console.log('[Service Worker] Aktivierung wird gestartet');
 	pEvent.waitUntil(
@@ -18,19 +29,35 @@ self.addEventListener('activate', function(pEvent) {
 	);
 });
 
-self.addEventListener('install', function(pEvent) {
-	console.log('[Service Worker] Installation wird gestartet');
-	pEvent.waitUntil(
-		caches.open(cacheName).then(function(pCache) {
-			console.log('[Service Worker] Dateien werden gecacht');
-			return pCache.addAll(filesToCache);
-		})
-	);
+/* Stale-while-revalidate Ansatz
+ * Seiten müssen nicht aktuellste sein, werden aus Cache geladen
+ * im Hintergrund wird offline nach aktuellerer Variante gesucht
+ */
+self.addEventListener('fetch', (event) => {
+	console.log("[Service Worker] Angeforderte URL:", event.request.url);
+	console.log("Die Ressource wurde aus dem Cache geladen und es wird im Netzwerk nach einer neuen Variante gesucht.");
+	event.respondWith(async function() {
+		const cache = await caches.open(cacheName);
+		const cachedResponse = await cache.match(event.request);
+		const networkResponsePromise = fetch(event.request);
+
+		event.waitUntil(async function() {
+			const networkResponse = await networkResponsePromise;
+			await cache.put(event.request, networkResponse.clone());
+		}());
+
+		// Return the cached response if we have one, otherwise return the network response.
+		if (cachedResponse) return cachedResponse;
+		
+		try {
+			return await networkResponsePromise;
+		} catch (err) {
+			return caches.match("/offline.html");
+		}
+	}());
 });
 
-self.addEventListener('fetch', function(pEvent) {
-	console.log('[Service Worker] Angeforderte URL:', pEvent.request.url);
-	pEvent.respondWith(
+/*	pEvent.respondWith(
 		caches.match(pEvent.request).then(function(pResponse) {
 			if (pResponse) {
 				console.log('[Service Worker] Ressource konnte aus dem Cache geladen werden:', pEvent.request.url);
@@ -41,7 +68,7 @@ self.addEventListener('fetch', function(pEvent) {
 			}
 		})
 	);
-});
+});*/
 
 // Background Sync
 self.addEventListener("sync", (event) => {
